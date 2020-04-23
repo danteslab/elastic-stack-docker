25b1abe10a37a62a2ddeff21f8756a57e15d9aca
Dante Calderon <mail@dantecalderon.dev>
2020-04-22 20:29:23 -0500

    Add matching examples

---
+++ src/using-matchers.spec.ts
@@ -0,0 +1,48 @@
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4)
+})
+
+test('object assignment', () => {
+  const data = { one: 1 }
+  data['two'] = 2
+  expect(data).toEqual({ one: 1, two: 2 })
+})
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0)
+    }
+  }
+})
+
+test('null', () => {
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
+})
+
+test('zero', () => {
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
+})
+
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK')
+}
+
+test('compiling android goes as expected', () => {
+  expect(compileAndroidCode).toThrow()
+  expect(compileAndroidCode).toThrow(Error)
+
+  // You can also use the exact error message or a regexp
+  expect(compileAndroidCode).toThrow('you are using the wrong JDK')
+  expect(compileAndroidCode).toThrow(/JDK/)
+})
